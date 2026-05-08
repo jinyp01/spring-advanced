43113bef2384c8cea40d48c89edf3510a8f1c721
jy030506@naver.com <jy030506@naver.com>
2026-05-08 17:00:36 +0900

    LV 3 : validation 사용하지 않는 import문, 메서드 추가 삭제

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -17,6 +16,4 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
 
     @EntityGraph(attributePaths = {"user"})
     Optional<Todo> findTodoById(Long todoId);
-
-    int countById(Long todoId);
 }
