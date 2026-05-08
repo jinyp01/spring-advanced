e5ac6cc2aca2a3df171676831d04489c219ab69d
jy030506@naver.com <jy030506@naver.com>
2026-05-08 16:58:09 +0900

    LV 3 : RefactoringQuiz - 3 validation @EntityGraph 식으로 변경

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -3,10 +3,12 @@ package org.example.expert.domain.todo.repository;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
@@ -18,6 +20,9 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
             "LEFT JOIN FETCH t.user " +
             "WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+
+    @EntityGraph(attributePaths = {"user"})
+    Optional<Todo> findTodoById(Long todoId);
 
     int countById(Long todoId);
 }
