fe0fee8d85f318593b02304b0d47b1b34da1b3f5
jy030506@naver.com <jy030506@naver.com>
2026-05-10 16:51:56 +0900

    LV 5 : Interceptor 클래스 추가 클래스 생성 후 WebMvcConfig에 등록

---
+++ src/main/java/org/example/expert/domain/common/interceptor/CheckAdminInterceptor.java
@@ -0,0 +1,19 @@
+package org.example.expert.domain.common.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class CheckAdminInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+
+    }
+}
