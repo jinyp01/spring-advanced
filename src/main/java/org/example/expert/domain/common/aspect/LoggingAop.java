b36f3c2c12400b8765a64558a7138c7d631a9bda
jy030506@naver.com <jy030506@naver.com>
2026-05-11 02:06:40 +0900

    Lv 5 : AOP를 사용한 로그 출력, 권한별 접근을 위한 SecurityConfig 파일 추가

--- src/main/java/org/example/expert/domain/common/aspect/LoggingAop.java
+++ src/main/java/org/example/expert/domain/common/aspect/LoggingAop.java
@@ -1,24 +1,49 @@
 package org.example.expert.domain.common.aspect;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Arrays;
 
 @Aspect
 @Slf4j
 @Component
 public class LoggingAop {
 
-    @Around("execution(* org.example.expert.domain.comment.service*(..))")
+    @Around("execution(* org.example.expert.domain.admin.controller*(..))")
     public Object commentAop(ProceedingJoinPoint joinPoint) throws Throwable {
-        long start = System.currentTimeMillis();
 
-        Object result = joinPoint.proceed(); // 실제 메서드 실행 -> Filter에서 doFilter 와 비슷함.
+        ServletRequestAttributes attr =
+                (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        HttpServletRequest request = attr.getRequest(); //
+        String requestBody = Arrays.toString(joinPoint.getArgs());
 
-        long end = System.currentTimeMillis();
-        log.info("[AOP] {} 실행됨 in {}ms" , joinPoint.getSignature() , end - start);
+        long start = System.currentTimeMillis(); // API 요청 시각
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String userId = auth.getName();
+
+        Object result = joinPoint.proceed(); // 응답 본문
+
+        log.info(" 유저 아이디 : {}," +
+                " API 요청 시각 : {}" +
+                " API 요청 URL : {}" +
+                " 요청 본문 : {}" +
+                " 응답 본문 : {}",
+                userId,
+                start,
+                request.getRequestURI(),
+                requestBody,
+                result);
+
         return result;
     }
 }
