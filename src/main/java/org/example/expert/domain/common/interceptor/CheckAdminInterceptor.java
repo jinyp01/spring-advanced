e13060a28bb03366edac384982a7e524d7f7267d
jy030506@naver.com <jy030506@naver.com>
2026-05-10 19:43:32 +0900

    LV 5 : 관리자 인증 성공 시 로그 출력

--- src/main/java/org/example/expert/domain/common/interceptor/CheckAdminInterceptor.java
+++ src/main/java/org/example/expert/domain/common/interceptor/CheckAdminInterceptor.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Slf4j
 @Component
@@ -18,12 +19,14 @@ public class CheckAdminInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
         UserRole userRole = UserRole.of(request.getAuthType());
+        LocalDateTime reqTime = LocalDateTime.now();
 
         if(!(userRole == UserRole.ADMIN)) {
             log.warn("관리자 아님. 접근 거부");
             response.sendError(HttpServletResponse.SC_FORBIDDEN, "관리자만 접근할 수 있습니다.");
             return false;
         }
+        log.info("관리자 인증 성공, 요청 시각 {}, 요청 URL {}", reqTime, request.getRequestURL());
 
         return true;
     }
