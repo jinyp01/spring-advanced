21854aee56023ab26b8ec60c2923548e694e664d
jy030506@naver.com <jy030506@naver.com>
2026-05-10 17:11:44 +0900

    LV 5 : Interceptor를 사용한 로깅 구현

--- src/main/java/org/example/expert/domain/common/interceptor/CheckAdminInterceptor.java
+++ src/main/java/org/example/expert/domain/common/interceptor/CheckAdminInterceptor.java
@@ -3,6 +3,8 @@ package org.example.expert.domain.common.interceptor;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.example.expert.domain.user.enums.UserRole;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -14,6 +16,14 @@ public class CheckAdminInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+        UserRole userRole = UserRole.of(request.getAuthType());
 
+        if(userRole == UserRole.ADMIN) {
+            log.warn("관리자 아님. 접근 거부");
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "관리자만 접근할 수 있습니다.");
+            return false;
+        }
+
+        return true;
     }
 }
