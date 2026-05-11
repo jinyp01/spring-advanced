9ec7e2df6856c87c5b288aece9091e5599f60f1e
jy030506@naver.com <jy030506@naver.com>
2026-05-11 11:03:45 +0900

    Lv 5 : AOP에서 요청본문과 응답본문이 제대로 출력되지 않는 현상 수정 objectMapper를 이용하여 json 형식으로 출력되도록 수정

--- src/main/java/org/example/expert/domain/common/aspect/LoggingAop.java
+++ src/main/java/org/example/expert/domain/common/aspect/LoggingAop.java
@@ -1,10 +1,13 @@
 package org.example.expert.domain.common.aspect;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -12,36 +15,44 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.Arrays;
+import java.util.logging.Logger;
 
 @Aspect
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class LoggingAop {
+
+    private final ObjectMapper objectMapper;
+
     @Around("execution(* org.example.expert.domain.admin.controller.AdminController.*(..))")
     public Object commentAop(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis(); // API 요청 시각
 
         ServletRequestAttributes attr =
                 (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
-        HttpServletRequest request = attr.getRequest(); //
-        String requestBody = Arrays.toString(joinPoint.getArgs());
+        HttpServletRequest request = attr.getRequest();
 
-        long start = System.currentTimeMillis(); // API 요청 시각
+        Object[] args = joinPoint.getArgs();
+        String requestBody = objectMapper.writeValueAsString(args); // 요청 본문
+
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String userId = auth.getName();
 
-        Object result = joinPoint.proceed(); // 응답 본문
+        Object result = joinPoint.proceed();
 
+        String responseBody = objectMapper.writeValueAsString(result); // 응답 본문
         log.info(" 유저 아이디 : {}," +
-                " API 요청 시각 : {}" +
-                " API 요청 URL : {}" +
-                " 요청 본문 : {}" +
-                " 응답 본문 : {}",
+                        " API 요청 시각 : {}" +
+                        " API 요청 URL : {}" +
+                        " 요청 본문 : {}" +
+                        " 응답 본문 : {}",
                 userId,
                 start,
                 request.getRequestURI(),
                 requestBody,
-                result);
+                responseBody);
 
         return result;
     }
